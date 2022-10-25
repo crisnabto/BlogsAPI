a22f19d20822c7f772e1c1d0d86117d3a5731c6b
Crisna Bezerra <crisnabto@gmail.com>
2022-10-25 16:12:39 -0300

    Requisitos 1 e 2

---
+++ src/migrations/20221025190645-create-posts-categories.js
@@ -0,0 +1,32 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('posts_categories', {
+      post_id: {
+        allowNull: false,
+        type: Sequelize.INTEGER,
+        onUpdate: 'CASCADE',
+        onDelete: 'CASCADE',
+        references: {
+          model: 'blog_posts',
+          key: 'id'
+        }
+      },
+      category_id: {
+        allowNull: false,
+        type: Sequelize.INTEGER,
+        onUpdate: 'CASCADE',
+        onDelete: 'CASCADE',
+        references: {
+          model: 'categories',
+          key: 'id'
+        }
+      }
+    })
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('post_categories');
+  }
+};
---
+++ src/models/User.js
@@ -0,0 +1,14 @@
+module.exports = (sequelize, DataTypes) => {
+    const User = sequelize.define('User', {
+      id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+      displayName: DataTypes.STRING,
+      email: DataTypes.STRING,
+      password: DataTypes.STRING,
+      image: DataTypes.STRING,
+    }, {
+      tableName: 'users',
+      underscored: true,
+    });
+  
+    return User;
+  };
